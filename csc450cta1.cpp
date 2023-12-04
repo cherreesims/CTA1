152806b1bf2f6fed2c7737b70692554b69486cab
cherreesims <145623229+cherreesims@users.noreply.github.com>
2023-12-03 21:14:27 -0600

    Update csc450cta1.cpp

--- csc450cta1.cpp
+++ csc450cta1.cpp
@@ -1,1 +1,22 @@
 
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	//This is the main fuction 
+	//Standard Output Statement
+	cout << "Welcome to this C++ Program" << endl;
+	//This outputs Welcome to this C++ Program
+
+	cout << "I have corrected all errors for this program." << endl;
+	//This statement outputs I have corrected all errors for this program 
+
+	//Wait for Output Screen
+
+return 0;
+//Added a return statement
+	//Main Function return statement
+}
+
+
